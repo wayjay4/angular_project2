b927e2e1da880b203b0ba578c8e711aabca1b28b
Waylon Dixon <wdixon@waylondixon.com>
2019-11-12 14:10:55 -0700

    using an 'inline template' vs 'templateURL'

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -2,7 +2,10 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-test',
-  templateUrl: './test.component.html',
+  //templateUrl: './test.component.html',
+  template: `<div>
+              <h3>Inline Template</h3>
+            </div>`,
   styleUrls: ['./test.component.css']
 })
 export class TestComponent implements OnInit {
