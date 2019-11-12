babfee30b60cbf10087929dc42c0907fbbe15caf
Waylon Dixon <wdixon@waylondixon.com>
2019-11-12 16:31:09 -0700

    Style Binding: ways of implementing style bindings

--- src/app/style-binding/style-binding.component.ts
+++ src/app/style-binding/style-binding.component.ts
@@ -5,6 +5,12 @@ import { Component, OnInit } from '@angular/core';
   template: `
     <div style="text_align:center;">
       <h2>Welcome, {{name}}!</h2>
+      <p [style.color]="'orange'">Style Binding</p>
+      <p [style.color]="hasError ? 'red': 'green'">Style Binding</p>
+
+      <p [style.color]="highlightColor">Style Binding 2</p>
+
+      <p [ngStyle]="titleStyles">Style Binding 3</p>
     </div>
   `,
   styles: []
@@ -13,6 +19,11 @@ export class StyleBindingComponent implements OnInit {
   public name = "RufusMace";
   public hasError = false;
   public isSpecial = true;
+  public highlightColor = "orange";
+  public titleStyles = {
+    color: "blue",
+    fontStyle: "italic",
+  };
 
   constructor() { }
 
