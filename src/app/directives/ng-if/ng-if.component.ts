a0869d5d5a58fcafa6ea52250724820a2d78d5ba
Waylon Dixon <wdixon@waylondixon.com>
2019-11-13 14:16:58 -0700

    ngIf Directive: rendering a <p> using the ngIF directive

--- src/app/directives/ng-if/ng-if.component.ts
+++ src/app/directives/ng-if/ng-if.component.ts
@@ -5,12 +5,15 @@ import { Component, OnInit } from '@angular/core';
   template: `
     <div>
       <h2>Welcome, {{name}}!</h2>
+
+      <p *ngIf="isDisplayElem">Should this paragraph be displayed?</p>
     </div>
   `,
   styleUrls: ['./ng-if.component.css']
 })
 export class NgIfComponent implements OnInit {
   public name = "RufusMace";
+  public isDisplayElem = false;
 
   constructor() { }
 
