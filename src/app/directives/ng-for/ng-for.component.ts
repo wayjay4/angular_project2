692882df12af39554d6d61e42ed98052797816a1
Waylon Dixon <wdixon@waylondixon.com>
2019-11-13 15:21:00 -0700

    ngFor Directive: using the ngFor directive

--- src/app/directives/ng-for/ng-for.component.ts
+++ src/app/directives/ng-for/ng-for.component.ts
@@ -5,13 +5,18 @@ import { Component, OnInit } from '@angular/core';
   template: `
     <div>
       <h2>Welcome, {{name}}!</h2>
+
+      <div *ngFor="let color of colorsArr">
+        <p>{{color}}</p>
+      </div>
     </div>
   `,
   styleUrls: ['./ng-for.component.css']
 })
 export class NgForComponent implements OnInit {
   public name = "RufusMace";
-  
+  public colorsArr = ["red", "blue", "green", "yellow"];
+
   constructor() { }
 
   ngOnInit() {
