b1404fd8f1ae34e8cbc579759ccff2ab44107ffe
Waylon Dixon <wdixon@waylondixon.com>
2019-11-13 16:20:13 -0700

    Component Interaction: sending Data from parent to child component using an ALIAS

--- src/app/directives/component-interaction/component-interaction.component.ts
+++ src/app/directives/component-interaction/component-interaction.component.ts
@@ -4,13 +4,14 @@ import { Component, OnInit, Input } from '@angular/core';
   selector: 'app-component-interaction',
   template: `
     <div>
-      <h2>Welcome, {{parentData}}!</h2>
+      <h2>Welcome, {{name}}!</h2>
     </div>
   `,
   styleUrls: ['./component-interaction.component.css']
 })
 export class ComponentInteractionComponent implements OnInit {
-  @Input () public parentData;
+  //@Input () public parentData;
+  @Input ('parentData') public name; // using an alias
 
   constructor() { }
 
