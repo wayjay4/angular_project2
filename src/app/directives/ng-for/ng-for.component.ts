0020a5ffcab7fc9448549131a2d875b933742195
Waylon Dixon <wdixon@waylondixon.com>
2019-11-13 15:45:23 -0700

    ngFor Directive: using the ngFor directive with FIRST, LAST, ODD, EVEN

--- src/app/directives/ng-for/ng-for.component.ts
+++ src/app/directives/ng-for/ng-for.component.ts
@@ -6,8 +6,29 @@ import { Component, OnInit } from '@angular/core';
     <div>
       <h2>Welcome, {{name}}!</h2>
 
+      <h3>ngFor using 'index'</h3>
       <div *ngFor="let color of colorsArr; index as i">
         <p>{{i}}: {{color}}</p>
+      </div><br /><br />
+
+      <h3>ngFor using 'first'</h3>
+      <div *ngFor="let color of colorsArr; first as f">
+        <p>{{f}}: {{color}}</p>
+      </div><br /><br />
+
+      <h3>ngFor using 'last'</h3>
+      <div *ngFor="let color of colorsArr; last as l">
+        <p>{{l}}: {{color}}</p>
+      </div><br /><br />
+
+      <h3>ngFor using 'odd'</h3>
+      <div *ngFor="let color of colorsArr; odd as o">
+        <p>{{o}}: {{color}}</p>
+      </div><br /><br />
+
+      <h3>ngFor using 'even'</h3>
+      <div *ngFor="let color of colorsArr; even as e">
+        <p>{{e}}: {{color}}</p>
       </div>
     </div>
   `,
