b10034b6fd53090ad8b58b559edd3da99f4fc462
Waylon Dixon <wdixon@waylondixon.com>
2019-11-13 14:53:11 -0700

    ngSwitch Directive: using the ngSwitch directive

--- src/app/directives/ng-switch/ng-switch.component.ts
+++ src/app/directives/ng-switch/ng-switch.component.ts
@@ -5,12 +5,19 @@ import { Component, OnInit } from '@angular/core';
   template: `
     <div>
       <h2>Welcome, {{name}}!</h2>
+
+      <div [ngSwitch]="color">
+        <p *ngSwitchCase="'red'">You picked the {{color}} color</p>
+        <p *ngSwitchCase="'blue'">You picked the {{color}} color</p>
+        <p *ngSwitchCase="'green'">You picked the {{color}} color</p>
+      </div>
     </div>
   `,
   styleUrls: ['./ng-switch.component.css']
 })
 export class NgSwitchComponent implements OnInit {
   public name = "RufusMace";
+  public color = "blue";
 
   constructor() { }
 
