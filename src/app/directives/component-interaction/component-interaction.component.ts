714dfcb26274b1e6898ea0673aec6c1a3ec6783f
Waylon Dixon <wdixon@waylondixon.com>
2019-11-13 16:25:24 -0700

    Component Interaction: sending Data from parent to child component sending an OBJECT {}

--- src/app/directives/component-interaction/component-interaction.component.ts
+++ src/app/directives/component-interaction/component-interaction.component.ts
@@ -4,14 +4,16 @@ import { Component, OnInit, Input } from '@angular/core';
   selector: 'app-component-interaction',
   template: `
     <div>
-      <h2>Welcome, {{name}}!</h2>
+      <h2>Welcome, {{parentData.name}}!</h2>
+      <p>your id is: {{parentData.id}}</p>
     </div>
   `,
   styleUrls: ['./component-interaction.component.css']
 })
 export class ComponentInteractionComponent implements OnInit {
   //@Input () public parentData;
-  @Input ('parentData') public name; // using an alias
+  //@Input ('parentData') public name; // using an alias
+  @Input () public parentData; // sending an data object {}
 
   constructor() { }
 
