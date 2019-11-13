e97407e461b8d3f65003842363f550ce977eb57c
Waylon Dixon <wdixon@waylondixon.com>
2019-11-13 14:28:46 -0700

    ngIf Directive: rendering a <p> using the ELSE block with the ngIF directive

--- src/app/directives/ng-if/ng-if.component.ts
+++ src/app/directives/ng-if/ng-if.component.ts
@@ -6,8 +6,17 @@ import { Component, OnInit } from '@angular/core';
     <div>
       <h2>Welcome, {{name}}!</h2>
 
-      <p *ngIf="isDisplayElem">Should this paragraph be displayed?</p>
+      <p *ngIf="isDisplayElem; else theElseBlock">Should this paragraph be displayed? I was hiding before, but the class var 'isDisplayElem' is set to 'true'.</p>
+
+      <ng-template #theElseBlock>
+        <p>There is a paragragh hidden because the class var 'isDisplayElem' is set to 'false'.</p>
+      </ng-template>
     </div>
+
+    <!--
+      1.) to add an else block, wrap the data in a 'ng-template' element using template reference var 'theElseBlock'
+      2.) reference the elseBlock in the ngIf directive
+    -->
   `,
   styleUrls: ['./ng-if.component.css']
 })
