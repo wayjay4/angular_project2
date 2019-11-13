62949da3373c0af3909b9e4e046cc8fdff25fa3e
Waylon Dixon <wdixon@waylondixon.com>
2019-11-13 15:33:27 -0700

    ngFor Directive: using the ngFor directive with INDEX i

--- src/app/directives/ng-for/ng-for.component.ts
+++ src/app/directives/ng-for/ng-for.component.ts
@@ -6,8 +6,8 @@ import { Component, OnInit } from '@angular/core';
     <div>
       <h2>Welcome, {{name}}!</h2>
 
-      <div *ngFor="let color of colorsArr">
-        <p>{{color}}</p>
+      <div *ngFor="let color of colorsArr; index as i">
+        <p>{{i}}: {{color}}</p>
       </div>
     </div>
   `,
