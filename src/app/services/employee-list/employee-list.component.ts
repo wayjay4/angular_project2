df64fe246964c24b8f8253157b81255e49d375dc
Waylon Dixon <wdixon@waylondixon.com>
2019-11-19 12:02:29 -0700

    Services: added employee list component

--- src/app/services/employee-list/employee-list.component.ts
+++ src/app/services/employee-list/employee-list.component.ts
@@ -6,6 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./employee-list.component.css']
 })
 export class EmployeeListComponent implements OnInit {
+  public employees = [
+    {"id": 1, "name": "Andrew", "age": 30},
+    {"id": 2, "name": "Brandon", "age": 25},
+    {"id": 3, "name": "Christina", "age": 26},
+    {"id": 4, "name": "Elena", "age": 28},
+  ];
 
   constructor() { }
 
