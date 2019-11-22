4119fe2753854ec410aece2112606d0d7912c033
Waylon Dixon <wdixon@waylondixon.com>
2019-11-22 10:32:47 -0700

    Services: saving changes

--- src/app/services/employee-list/employee-list.component.ts
+++ src/app/services/employee-list/employee-list.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit, Injectable } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { EmployeeService } from '../shared/employee.service';
 
 @Component({
   selector: 'app-employee-list',
   templateUrl: './employee-list.component.html',
-  styleUrls: ['./employee-list.component.css'],
+  styleUrls: ['./employee-list.component.css']
 })
 export class EmployeeListComponent implements OnInit {
   public employees = [];
 
-  constructor(public _employeeService: EmployeeService) { }
+  constructor(private _employeeService: EmployeeService) { }
 
   ngOnInit() {
     this.employees = this._employeeService.getEmployees();
