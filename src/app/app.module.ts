085df2ab581cd523332f4bf9392ebe49cb2b2ed2
Waylon Dixon <wdixon@waylondixon.com>
2019-12-02 13:36:38 -0700

    Fetch Data Using HTTP: implemented HttpClient, Observable, IEmployee interface

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
@@ -35,7 +36,8 @@ import { EmployeeService } from './services/shared/employee.service';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [EmployeeService],
   bootstrap: [AppComponent]
--- src/app/services/shared/employee.service.ts
+++ src/app/services/shared/employee.service.ts
@@ -1,18 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { IEmployee } from './employee';
+import { Observable } from 'rxjs/Observable'; 
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
+  private _url: string = "/assets/data/employees.json";
 
-  constructor(){}
+  constructor(private http: HttpClient){}
 
-  getEmployees(){
-    return [
-      {"id": 1, "name": "Andrew", "age": 30},
-      {"id": 2, "name": "Brandon", "age": 25},
-      {"id": 3, "name": "Christina", "age": 26},
-      {"id": 4, "name": "Elena", "age": 28}
-    ];
+  getEmployees(): Observable<IEmployee[]>{
+    return this.http.get<IEmployee[]>(this._url);
   }
 }
